dea5477d80b0d9fd07b3996e4b6229b0b0ef4607
djneces <j.necesanek@hotmail.com>
2021-07-09 15:06:03 +0200

    Fixed ReferenceError

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const path = require('path');
+const express = require('express');
 
 const mongoose = require('mongoose');
 
@@ -52,7 +53,6 @@ if (process.env.NODE_ENV === 'production') {
   //Express will serve up production assets (main.js or main.css)
   app.use(express.static('client/build'));
   //Express will serve up the index.html if it doesn't recognize the route
-  const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
